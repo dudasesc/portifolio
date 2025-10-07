8f92e5ba3c9ef204d9750132ead6ca5d49714e54
dudasesc <vieira.6995@aluno.pr.senac.br>
2025-10-07 15:30:29 -0300

    script.js

--- script.js
+++ script.js
@@ -32,4 +32,10 @@ window.onscroll = function() {
 document.getElementById("backToTop").addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+});
 
